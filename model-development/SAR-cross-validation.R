49ed0e457a2f57bdb6c8df782a28841cdf8f8376
Kristine Dinh <kdinh@arrowheadgrp.com>
2020-04-20 21:48:58 -0700

    updated model

--- model-development/SAR-cross-validation.R
+++ model-development/SAR-cross-validation.R
@@ -13,38 +13,53 @@ fire <- read_rds("data/data.RDS")
 
 CA <- counties(state = "CA", resolution = "5m")
 
-fire %<>%
-  group_by(COUNTYFP) %>%
-  summarise(fire_volume = sum(fire_volume)/1000000,
-            fire_freq = n(),
-            discovery_max_temp = mean(discovery_max_temp))
+# fire %<>%
+#   group_by(COUNTYFP) %>%
+#   summarise(fire_volume = sum(fire_volume)/1000000,
+#             fire_freq = n(),
+#             discovery_max_temp = mean(discovery_max_temp))
+# 
+# CA_fire <- inner_join(as.data.frame(CA), fire, by = "COUNTYFP")
+# 
+# CA_fire %<>% modify_at(c("INTPTLON", "INTPTLAT"), as.numeric)
+# 
+# coordinates(fire) <- c("lon", "lat")
+# 
+# proj4string(fire) <- CA@proj4string
+# 
+# fire_new <- over(CA, fire)
+# 
+# fire@data
 
-CA_fire <- inner_join(as.data.frame(CA), fire, by = "COUNTYFP")
-
-CA_fire %<>% modify_at(c("INTPTLON", "INTPTLAT"), as.numeric)
-
-coordinates(fire) <- c("lon", "lat")
-
-proj4string(fire) <- CA@proj4string
-
-fire_new <- over(CA, fire)
-
-fire@data
 
 
-nb_CA <- poly2nb(CA_fire)
+
+
+# u <- union(fire$COUNTYFP, CA$COUNTYFP)
+
+# CA@data %<>% filter(COUNTYFP %in% c(intersect(fire$COUNTYFP, CA$COUNTYFP)))
+
+CA <- CA[CA$COUNTYFP %in% intersect(fire$COUNTYFP, CA$COUNTYFP), ]
+
+CA$fire_volume <- sapply(as.character(unique(fire$COUNTYFP)), function(x){
+  sum(fire$fire_volume[fire$COUNTYFP == x], na.rm = T)
+})
+
+CA$discovery_max_temp <- sapply(as.character(unique(fire$COUNTYFP)), function(x){
+  median(fire$discovery_max_temp[fire$COUNTYFP == x], na.rm = T)
+})
+
+nb_CA <- poly2nb(CA)
 
 plot(CA)
 
 plot(nb_CA, coordinates(CA), add = T)
-
-
 
 listW <- nb2listw(nb_CA, style = "B")
 
 W <- listw2mat(listW)
 
 SAR_fire <- spautolm(fire_volume ~ discovery_max_temp, 
-                     data = CA_fire, 
+                     data = CA, 
                      listw = listW, family = "SAR")
 
